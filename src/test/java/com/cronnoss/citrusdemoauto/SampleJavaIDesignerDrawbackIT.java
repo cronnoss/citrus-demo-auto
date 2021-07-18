71b7f5452791ef00e2824ea505919cd250f45e3d
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-18 14:46:37 +0300

    04 - Citrus TestDesigner Drawbacks

---
+++ src/test/java/com/cronnoss/citrusdemoauto/SampleJavaIDesignerDrawbackIT.java
@@ -0,0 +1,27 @@
+package com.cronnoss.citrusdemoauto;
+
+import com.consol.citrus.actions.AbstractTestAction;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import org.testng.annotations.Test;
+import utils.LoggingService;
+
+@Test
+public class SampleJavaIDesignerDrawbackIT extends TestNGCitrusTestDesigner {
+
+    LoggingService obj = new LoggingService();
+
+    @CitrusTest
+    public void testLogService() {
+        echo("Before log called");
+        action(new AbstractTestAction() {
+
+            @Override
+            public void doExecute(TestContext context) {
+                obj.logs("Logs will be printed...");
+            }
+        });
+        echo("After log called");
+    }
+}
