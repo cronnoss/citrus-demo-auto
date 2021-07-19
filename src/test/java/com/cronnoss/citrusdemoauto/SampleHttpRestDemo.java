3b83be5716f2c43cbd9b1442a81e0d7b83fab665
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-19 20:42:34 +0300

    add control value in validation

--- src/test/java/com/cronnoss/citrusdemoauto/SampleHttpRestDemo.java
+++ src/test/java/com/cronnoss/citrusdemoauto/SampleHttpRestDemo.java
@@ -23,6 +23,7 @@ public class SampleHttpRestDemo extends TestNGCitrusTestDesigner {
                 .header("Authorization", "Basic YWRtaW5AY3Jvbm5vc3MuY29tOmFkbWlu");
         http().client(user).receive().response(HttpStatus.OK)
                 .validate("$", notNullValue())
+                .validate("$", "{\"_embedded\":{\"users\":[{\"firstName\":\"User_First\",\"lastName\":\"User_Last\",\"_links\":{\"self\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\\/1\"},\"user\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\\/1\"}},\"roles\":[\"USER\"],\"email\":\"user@gmail.com\"},{\"firstName\":\"Admin_First\",\"lastName\":\"Admin_Last\",\"_links\":{\"self\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\\/2\"},\"user\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\\/2\"}},\"roles\":[\"USER\",\"ADMIN\"],\"email\":\"admin@cronnoss.com\"}]},\"_links\":{\"search\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\\/search\"},\"profile\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/profile\\/users\"},\"self\":{\"href\":\"http:\\/\\/localhost:8080\\/api\\/users\"}},\"page\":{\"number\":0,\"size\":20,\"totalPages\":1,\"totalElements\":2}}")
                 .extractFromPayload("$", "response");
         echo("${response}");
     }
