baa52ec4517b26d09e082f3fdea615c288f4229c
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-19 21:38:46 +0300

    add testAddUser

--- src/test/java/com/cronnoss/citrusdemoauto/SampleHttpRestDemo.java
+++ src/test/java/com/cronnoss/citrusdemoauto/SampleHttpRestDemo.java
@@ -54,7 +54,7 @@ public class SampleHttpRestDemo extends TestNGCitrusTestDesigner {
         echo("${response}");
     }
 
-    @Test
+    @Test(enabled = false)
     @CitrusTest
     public void testGetUserById() {
 
@@ -84,6 +84,40 @@ public class SampleHttpRestDemo extends TestNGCitrusTestDesigner {
         http().client(user).receive().response(HttpStatus.OK)
                 .validate("$", notNullValue())
                 .validate("$", data)
+                .extractFromPayload("$", "response");
+        echo("${response}");
+    }
+
+    @Test
+    @CitrusTest
+    public void testAddUser() {
+
+        String data = "";
+        ClassPathResource resource = new ClassPathResource("userAdd.json");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            data = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        http().client(user).send()
+                .post()
+                .contentType("application/json")
+                .payload(resource)
+                .header("Authorization", "Basic YWRtaW5AY3Jvbm5vc3MuY29tOmFkbWlu");
+        http().client(user).receive().response(HttpStatus.CREATED)
                 .extractFromPayload("$", "response");
         echo("${response}");
     }
